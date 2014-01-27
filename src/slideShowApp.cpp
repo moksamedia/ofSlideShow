f278729bb6bdd1ce56a867fdb242850d6edb8369
Andrew Hughes <andrewcarterhughes@gmail.com>
2014-01-27 10:21:57 -0600

    initial commit

---
+++ src/slideShowApp.cpp
@@ -0,0 +1,61 @@
+#include "slideShowApp.h"
+
+//--------------------------------------------------------------
+void slideShowApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void slideShowApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
